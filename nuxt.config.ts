42ea30f0d310fc85e36a0c03f07b3ed64ca1b5d3
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-06-18 23:24:20 +0200

    chore: wip i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,16 +41,6 @@ export default defineNuxtConfig({
     redirectToCanonicalSiteUrl: true,
   },
   i18n: {
-    legacy: false,
-    locale: "en",
-    locales: ["en", "fr"],
-    messages: {
-      en: {
-        welcome: "Welcome",
-      },
-      fr: {
-        welcome: "Bienvenue",
-      },
-    },
+    vueI18n: "./i18n.config.ts",
   },
 });
