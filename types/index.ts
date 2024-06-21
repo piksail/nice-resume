d0257645dafd6c0954d1205c1d08529b00f0244e
Benjent <benjent@hotmail.fr>
2024-06-21 23:43:30 +0300

    feat: add new entry layout

--- types/index.ts
+++ types/index.ts
@@ -161,7 +161,7 @@ export type ResumeSettings = CommonDocumentSettings & {
     width?: number | "fit";
   };
   entry: BlockSettings & {
-    layout: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+    layout: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
     gap: number; // Flex gap between entries
   };
   entryTitle: TextSettings & {
