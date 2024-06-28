0afebabad76e64ddc24e5268f0961cf49e107b9f
Benjent <benjent@hotmail.fr>
2024-06-28 19:15:25 +0300

    fix(landing.vue): blur on header bg

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -5,7 +5,7 @@
     class="bgGradient backdrop-contrast-200 text-white min-h-[100vh] scroll-smooth"
   >
     <header
-      class="sticky top-0 z-10 h-[100px] flex justify-between items-center gap-2 px-10 text-white border-white border-b-2 border-opacity-5"
+      class="sticky top-0 z-10 h-[100px] flex justify-between items-center gap-2 px-10 text-white border-white border-b-2 border-opacity-5 backdrop-blur-sm backdrop-filter"
     >
       <NuxtLink to="/">
         <h1 class="text-center text-4xl font-black tracking-widest uppercase">
