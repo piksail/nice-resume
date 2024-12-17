5d0456adfc10bee2852a653ec6d0d5cb71b86a3c
Benjent <benjent@hotmail.fr>
2024-12-17 09:56:50 +0200

    feat: do not make header title an h1

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -26,11 +26,14 @@ const regionCodeFromLocale: { [key in LocaleCode]: RegionCode } = {
       class="sticky top-0 z-10 h-[100px] flex justify-between items-center gap-2 px-10 text-white border-white border-b-2 border-opacity-5 backdrop-blur backdrop-filter"
     >
       <NuxtLink :to="localePath('/')">
-        <h1 class="text-center text-4xl font-black tracking-widest uppercase">
+        <p
+          class="text-center text-4xl font-black tracking-widest uppercase"
+          data-testid="headerTitle"
+        >
           Nice
           <br />
           Resume
-        </h1>
+        </p>
       </NuxtLink>
       <div class="flex items-center gap-3 h-1/4">
         <RegionFlag
