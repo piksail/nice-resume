28274c3b0a96c74e6ae1517db12dd8ac29458a67
Benjent <benjent@hotmail.fr>
2025-02-13 19:05:54 +0200

    feat: grid layout for homepage steps

--- pages/about.vue
+++ pages/about.vue
@@ -21,37 +21,10 @@ const { t } = useI18n({
   </Head>
   <NuxtLayout name="landing">
     <section class="p-20">
-      <section class="p-20">
-        <Card>
-          <p class="text-4xl font-bold">
-            {{ t("stepTheme") }}
-            <br />
-            {{ t("stepResume") }}
-            <br />
-            {{ t("stepLetter") }}
-            <br />
-            {{ t("stepCustomize") }}
-          </p>
-          <p class="text-2xl font-semibold mt-8">
-            It's
-            <i class="font-serif">that</i>
-            simple
-          </p>
-          <NuxtLink :to="localePath('/editor')">
-            <Button class="uppercase font-black tracking-widest mx-auto mt-8">
-              {{ t("getStarted") }}
-            </Button>
-          </NuxtLink>
-        </Card>
-        <div class="max-w-screen-md mx-auto">
-          <p class="text-lg font-thin text-right">
-            No cookies. No signup. No payment. Privacy-friendly.
-          </p>
-        </div>
-      </section>
       <section class="p-20 max-w-screen-xl mx-auto">
         <h2 class="drop-shadow-lg text-5xl font-bold mb-32">
-          {{ t("stepTheme") }}
+          Handcrafted themes + famous themes from JSONResume.org, OpenResume,
+          ...
           <i class="font-serif text-primary">{{ $t("theme") }}</i>
         </h2>
         <div class="relative h-[calc((400px*1.414)+50px)] mx-auto group">
@@ -104,82 +77,8 @@ const { t } = useI18n({
           </NuxtLink>
         </div>
       </section>
-      <section class="p-20 max-w-screen-xl mx-auto">
-        <h2 class="drop-shadow-lg text-5xl font-bold mb-32">
-          {{ t("stepLetter") }}
-          <i class="font-serif text-primary">{{ $t("coverLetter") }}</i>
-        </h2>
-        <div class="flex gap-10 relative">
-          <img
-            src="/macchiato-letter-example.png"
-            alt="Example of a cover letter written and styled with Nice Resume"
-            class="w-[400px] h-[calc(400px*1.414)] bg-white shadow hover:scale-105 transition-all"
-          />
-          <img
-            src="/letter-editor.png"
-            alt="Screenshot of the cover letter editor"
-            class="h-[calc(400px*1.414)] bg-white shadow hover:scale-105 transition-all"
-          />
-          <Card
-            class="absolute right-32 bottom-6 backdrop-blur backdrop-filter"
-          >
-            <p
-              class="text-3xl relative after:content-[''] after:relative after:-left-2 after:top-1 after:h-9 after:w-[2px] after:bg-white after:inline-block after:animate-cursor-blink"
-            >
-              Regards,
-              <br />
-              Firstname Lastname
-            </p>
-          </Card>
-          <NuxtLink :to="localePath('/editor')">
-            <Button class="uppercase font-black tracking-widest mx-auto mt-8">
-              {{ t("getStarted") }}
-            </Button>
-          </NuxtLink>
-        </div>
-      </section>
-      <section class="p-20 max-w-screen-xl mx-auto">
-        <h2 class="drop-shadow-lg text-5xl font-bold mb-32 text-right">
-          {{ t("stepEmail") }}
-          <i class="font-serif text-primary">{{ $t("email") }}</i>
-        </h2>
-        <div class="relative mx-auto flex group">
-          <img
-            src="/email-editor.png"
-            alt="Screenshot of the email editor"
-            class="w-[600px] bg-white shadow group-hover:scale-110 group-hover:shadow-lg transition-all"
-          />
-          <img
-            src="/macchiato-email-example.png"
-            alt="Example of an email signed with Nice Resume"
-            class="absolute left-14 top-14 w-[600px] bg-white shadow group-hover:scale-110 group-hover:shadow-lg group-hover:translate-x-3/4 transition-all"
-          />
-          <NuxtLink :to="localePath('/editor')">
-            <Button class="uppercase font-black tracking-widest mx-auto mt-8">
-              {{ t("getStarted") }}
-            </Button>
-          </NuxtLink>
-        </div>
-      </section>
-      <section class="p-20 max-w-screen-xl mx-auto">
-        <h2 class="drop-shadow-lg text-5xl font-bold mb-32">
-          {{ t("stepCustomize") }}
-          <i class="font-serif text-primary">{{ $t("design") }}</i>
-        </h2>
-        <div class="relative mx-auto group">
-          <img
-            src="/style-editor.png"
-            alt="Screenshot of the style editor"
-            class="w-[600px] bg-white shadow group-hover:scale-110 group-hover:shadow-lg transition-all"
-          />
-          <NuxtLink :to="localePath('/editor')">
-            <Button class="uppercase font-black tracking-widest mx-auto mt-8">
-              {{ t("getStarted") }}
-            </Button>
-          </NuxtLink>
-        </div>
-      </section>
       <section class="p-20 max-w-screen-2xl mx-auto">
+        TODO expliquer que y avait besoin d'un enième CV builder
         <h2 class="drop-shadow-lg text-5xl font-bold mb-32">
           {{ t("todo") }}
           <i class="font-serif text-primary">{{ $t("todo") }}</i>
@@ -189,48 +88,3 @@ const { t } = useI18n({
     </section>
   </NuxtLayout>
 </template>
-
-<i18n lang="json">
-{
-  "br": {
-    "stepTheme": "TODO",
-    "stepResume": "TODO",
-    "stepLetter": "TODO",
-    "stepEmail": "TODO",
-    "stepCustomize": "TODO",
-    "getStarted": "TODO!"
-  },
-  "de": {
-    "stepTheme": "Wählen Sie ein",
-    "stepResume": "Erstellen Irhe",
-    "stepLetter": "Schreiben Ihre",
-    "stepEmail": "Zeichen Ihre",
-    "stepCustomize": "Anpassen die",
-    "getStarted": "Anfangen"
-  },
-  "en": {
-    "stepTheme": "Pick a",
-    "stepResume": "Build your",
-    "stepLetter": "Write your",
-    "stepEmail": "Sign your",
-    "stepCustomize": "Adapt the",
-    "getStarted": "Get started!"
-  },
-  "es": {
-    "stepTheme": "TODO",
-    "stepResume": "TODO",
-    "stepLetter": "TODO",
-    "stepEmail": "TODO",
-    "stepCustomize": "TODO",
-    "getStarted": "TODO"
-  },
-  "fr": {
-    "stepTheme": "Choisissez un",
-    "stepResume": "Composez votre",
-    "stepLetter": "Rédigez votre",
-    "stepEmail": "Signez votre",
-    "stepCustomize": "Adaptez le",
-    "getStarted": "C'est parti\xa0!"
-  }
-}
-</i18n>
--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@ const thirdStyleInput = ref(400);
   </Head>
 
   <NuxtLayout name="landing">
-    <section class="p-20 pb-0 flex justify-center">
+    <section class="p-20 flex justify-center">
       <div class="mx-auto">
         <h1 class="text-justify drop-shadow-lg">
           <span class="text-9xl font-bold tracking-wide">
@@ -140,6 +140,122 @@ const thirdStyleInput = ref(400);
         </div>
       </div>
     </section>
+    <section class="max-w-screen-2xl mx-auto">
+      <ol class="grid grid-cols-3 grid-rows-2 grid-flow-col gap-3">
+        <li class="border-2 border-surface-800 row-span-2">
+          <h2 class="text-4xl font-bold mb-32">
+            <Badge size="xlarge" value="1" />
+            {{ t("stepTheme") }}
+            <i class="font-serif text-primary">{{ $t("theme") }}</i>
+          </h2>
+          <div class="grid grid-cols-3">
+            <img
+              src="/paper-example.png"
+              alt="First example of a resume built with Nice Resume"
+              class=""
+            />
+            <img
+              src="/macchiato-example.png"
+              alt="Second example of a resume built with Nice Resume"
+              class=""
+            />
+            <img
+              src="/stone-example.png"
+              alt="Third example of a resume built with Nice Resume"
+              class=""
+            />
+          </div>
+        </li>
+        <li class="border-2 border-surface-800">
+          <h2 class="text-4xl font-bold mb-32">
+            <Badge size="xlarge" value="2" />
+            {{ t("stepEdit") }}
+            <i class="font-serif text-primary">{{ $t("resume") }}</i>
+          </h2>
+          <div class="relative mx-auto group">
+            <!-- <img
+              src="/profile-editor.png"
+              alt="Screenshot of the profile editor"
+              class="w-[600px] bg-white shadow group-hover:scale-110 group-hover:shadow-lg transition-all"
+            /> -->
+            <div
+              class="grid grid-cols-3 gap-4 items-end bg-surface-800 p-6 rounded"
+            >
+              <Field
+                id="introFirstInput"
+                :label="$t('name')"
+                transparent
+                v-model="firstInput"
+                class="[&_input]!bg-white"
+              />
+              <Field
+                id="introSecondInput"
+                :label="$t('phone')"
+                transparent
+                v-model="secondInput"
+              />
+              <Field
+                id="introThirdInput"
+                :label="$t('address')"
+                transparent
+                v-model="thirdInput"
+              />
+            </div>
+          </div>
+        </li>
+        <li class="border-2 border-surface-800">
+          <h2 class="text-4xl font-bold mb-32">
+            <Badge size="xlarge" value="3" />
+            {{ t("stepCustomize") }}
+            <i class="font-serif text-primary">{{ $t("design") }}</i>
+          </h2>
+          <div
+            class="grid grid-cols-3 gap-4 items-end bg-surface-800 p-6 rounded"
+          >
+            <Field
+              id="introFirstStyleInput"
+              :label="$t('color')"
+              transparent
+              v-model="firstStyleInput"
+              type="color"
+            />
+            <Field
+              id="introSecondStyleInput"
+              :label="$t('italic')"
+              transparent
+              v-model="secondStyleInput"
+              type="checkbox"
+            />
+            <Field
+              id="introThirdStyleInput"
+              :label="$t('fontWeight')"
+              transparent
+              v-model="thirdStyleInput"
+              type="select"
+              :options="[100, 200, 300, 400, 500, 600, 700, 800, 900]"
+            />
+          </div>
+        </li>
+        <li class="border-2 border-surface-800 row-span-2">
+          <h2 class="text-4xl font-bold mb-32">
+            <Badge size="xlarge" value="4" />
+            {{ t("stepPreview") }}
+            <i class="font-serif text-primary">{{ $t("preview") }}</i>
+          </h2>
+          <div class="w-[400px] h-[calc(400px*1.414)] shadow bg-white">
+            <span :style="{ color: firstStyleInput }">{{ firstInput }}</span>
+            <span
+              :style="{ fontStyle: secondStyleInput ? 'italic' : 'normal' }"
+            >
+              {{ secondInput }}
+            </span>
+            <span :style="{ fontWeight: thirdStyleInput }">
+              {{ thirdInput }}
+            </span>
+          </div>
+        </li>
+      </ol>
+    </section>
     <NuxtPage />
   </NuxtLayout>
 </template>
@@ -153,7 +269,11 @@ const thirdStyleInput = ref(400);
     "introIndefiniteArticle": "TODO",
     "introPowerWord": "TODO",
     "startEditing": "TODO",
-    "learnMore": "TODO"
+    "learnMore": "TODO",
+    "stepTheme": "TODO",
+    "stepEdit": "TODO",
+    "stepCustomize": "TODO",
+    "stepPreview": "TODO"
   },
   "de": {
     "introVerb": "Machen sie",
@@ -162,7 +282,11 @@ const thirdStyleInput = ref(400);
     "introIndefiniteArticle": "zu einem",
     "introPowerWord": "Kunstwerk",
     "startEditing": "Anfangen",
-    "learnMore": "TODO"
+    "learnMore": "TODO",
+    "stepTheme": "Wählen Sie ein",
+    "stepEdit": "Erstellen Irhe",
+    "stepCustomize": "Anpassen die",
+    "stepPreview": "TODO"
   },
   "en": {
     "introVerb": "Make",
@@ -171,7 +295,11 @@ const thirdStyleInput = ref(400);
     "introIndefiniteArticle": "a",
     "introPowerWord": "brand",
     "startEditing": "Start editing",
-    "learnMore": "Learn more"
+    "learnMore": "Learn more",
+    "stepTheme": "Pick a",
+    "stepEdit": "Edit your",
+    "stepCustomize": "Adapt the",
+    "stepPreview": "TODO"
   },
   "es": {
     "introVerb": "TODO",
@@ -180,7 +308,11 @@ const thirdStyleInput = ref(400);
     "introIndefiniteArticle": "TODO",
     "introPowerWord": "TODO",
     "startEditing": "TODO",
-    "learnMore": "TODO"
+    "learnMore": "TODO",
+    "stepTheme": "TODO",
+    "stepEdit": "TODO",
+    "stepCustomize": "TODO",
+    "stepPreview": "TODO"
   },
   "fr": {
     "introVerb": "De",
@@ -189,7 +321,11 @@ const thirdStyleInput = ref(400);
     "introIndefiniteArticle": "une",
     "introPowerWord": "œuvre",
     "startEditing": "Commencer à éditer",
-    "learnMore": "En savoir plus"
+    "learnMore": "En savoir plus",
+    "stepTheme": "Choisissez un",
+    "stepEdit": "Editez votre",
+    "stepCustomize": "Adaptez le",
+    "stepPreview": "TODO"
   }
 }
 </i18n>
