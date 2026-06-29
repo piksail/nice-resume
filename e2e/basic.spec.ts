5a6915b288fbdfffd1966abe290399b8ced026ae
Benjent <benjent@hotmail.fr>
2026-06-29 16:46:00 +0300

    fix: e2e tests

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
+import { APP_NAME } from "~/globals";
 
 test("test", async ({ page }) => {
   await page.goto("/");
-  await expect(page.getByText("Nice Resume")).toBeVisible();
+  await expect(page.getByText(APP_NAME)).toBeVisible();
 });
